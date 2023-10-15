a457fdce87e64dd94a98c654ba630b33c2c227ae
rudhendra-sahoo <147960011+rudhendra-sahoo@users.noreply.github.com>
2023-10-15 20:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function convertTemperature() {
+    const inputTemp = parseFloat(document.getElementById("inputTemp").value);
+    const fromUnit = document.getElementById("fromUnit").value;
+    const toUnit = document.getElementById("toUnit").value;
+
+    let result;
+
+    if (fromUnit === "celsius" && toUnit === "fahrenheit") {
+        result = (inputTemp * 9/5) + 32;
+    } else if (fromUnit === "fahrenheit" && toUnit === "celsius") {
+        result = (inputTemp - 32) * 5/9;
+    } else {
+        result = inputTemp;
+    }
+
+    document.getElementById("result").textContent = `Result: ${result.toFixed(2)} ${toUnit}`;
+}
+
+convertTemperature();
